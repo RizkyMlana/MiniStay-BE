834d8b59e40f119903ed9de3df609426c2a155fc
RizkyMaulanaS <rizkyzky345@gmail.com>
2025-12-05 14:58:31 +0700

    test

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
