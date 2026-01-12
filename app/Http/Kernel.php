bd9064947b199a5d41da9b700eec3fabe83b0b15
RizkyMaulanaS <rizkyzky345@gmail.com>
2026-01-12 11:32:35 +0700

    add middlewares

---
+++ app/Http/Kernel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     * Minimal untuk API Laravel.
+     */
+    protected $middleware = [
+        // Bisa tambahkan middleware global jika perlu
+    ];
+
+    /**
+     * The application's route middleware groups.
+     */
+    protected $middlewareGroups = [
+        'api' => [
+            // Minimal untuk API
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+
+    /**
+     * The application's route middleware.
+     */
+    protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    ];
+}
--- app/Http/Middleware/Verifycsrftoken.php
+++ app/Http/Middleware/Verifycsrftoken.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
