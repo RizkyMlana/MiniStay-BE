9a2416308b2b07df3efbb4cd7e303422afe2bc20
RizkyMaulanaS <rizkyzky345@gmail.com>
2026-01-20 00:05:27 +0700

    update room

--- routes/api.php
+++ routes/api.php
@@ -64,7 +64,7 @@ Route::middleware(['auth:sanctum', RoleMiddleware::class. ':admin'])
 
     // ROOM BLOCKS
     Route::post('/room-blocks', [RoomBlockController::class, 'store']);
-    Route::get('/room-date', [RoomBlockController::class, 'getRoomBlocks']);
+    Route::get('/room-date/{roomId}', [RoomBlockController::class, 'getRoomBlocks']);
 
     // BOOKINGS (ADMIN)
     Route::get('/bookings', [BookingController::class, 'index']);
