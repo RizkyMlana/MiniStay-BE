bb2b0093cba1d632d3953426a2fe20ee09434507
RizkyMaulanaS <rizkyzky345@gmail.com>
2026-01-20 19:50:58 +0700

    update

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -106,11 +106,19 @@ class BookingController extends Controller
     }
 
 
-    public function index(){
-        return Booking::with(['user', 'room'])
-            ->orderByDesc('created_at')
-            ->get();
+    public function index(Request $request){
+        $query = Booking::with(['user', 'room'])->orderByDesc('created_at');
+
+        if($request->has('status')){
+            $query->where('status', $request->status);
+        }
+        if($request->has('from_date') && $request->has('to_date')){
+            $query->whereBetween('check_in_date', [$request->from_date, $request->to_date]);
+        }
+
+        return $query->paginate(20);
     }
+
 
     public function cancel($id){
         $booking = Booking::findOrFail($id);
